eeddbc1ea49bc3a6c8c57a5861daaf22a3eff357
Martin Rocek <martin@rocek.dev>
2021-10-12 18:35:34 +0200

    ✨ (cli): add yargs and config for better customizability

--- exporter.js
+++ exporter.js
@@ -1,43 +1,154 @@
 import xlsx from 'xlsx'
+import fs from 'fs'
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+import { exit } from 'process'
 
-const file = xlsx.readFile('./FILENAME.xlsx')
-const sheet = file.Sheets[file.SheetNames[0]]
-const range = xlsx.utils.decode_range(sheet['!ref'])
+const argv = yargs(hideBin(process.argv))
+  .option('input', {
+    alias: 'i',
+    describe: 'The input file to be converted to xlsx',
+    type: 'string'
+  })
+  .option('output', {
+    alias: 'o',
+    describe: 'The output file to be created',
+    type: 'string',
+  })
+  .option('config', {
+    alias: 'c',
+    describe: 'The config file to be used',
+    type: 'string',
+  })
+  .help()
+  .alias('help', 'h')
+  .argv
 
-const cleanStandardFormatting = (text) => {
-  const defaultFormatting = text.replace(/(<span style="font-size:12pt;">)(.*?)(<\/span>)/g, '$2')
-  return defaultFormatting
+if (!argv.input) {
+  // Fail if no input file is specified.
+  console.info('Please provide an input file')
+  exit(0)
 }
 
-const appendTopicTags = (text) => {
-  const defaultFormatting = text.replace(/(<span style="font-size:9pt;">)(.*?)(<\/span>)/g, '<topic>$2</topic>')
-  return defaultFormatting
+// Set global variables
+const INPUT_FILE = argv.input
+const OUTPUT_FILE = argv.output || argv.input.replace('.xlsx', '.xml')
+let CONFIG = null
+if (argv.config) {
+  CONFIG = JSON.parse(fs.readFileSync(argv.config))
 }
 
-const appendAppendix = (text) => {
-  const colonPosition = text.indexOf(': ')
+/**
+ * Remove selected tags from the text.
+ *
+ * @param {string} text value to be cleaned
+ * @returns
+ */
+const stripTags = (text) => {
+  if (CONFIG.stripTags.length === 0) {
+    return text
+  }
+
+  let cleanedText = text
+  for (const tag of CONFIG.stripTags) {
+    const startTag = tag
+    const endTag = `</${tag.slice(1, tag.indexOf(' '))}>`
+    const regex = new RegExp(`(${startTag})(.*?)(${endTag})`, 'g')
+
+    cleanedText = cleanedText.replace(regex, '$2')
+  }
+
+  return cleanedText
+}
+
+/**
+ * Replace selected tags from the text.
+ *
+ * @param {string} text value to be cleaned
+ * @returns
+ */
+const replaceTags = (text) => {
+  if (CONFIG.replaceTags.length === 0) {
+    return text
+  }
+
+  let cleanedText = text
+
+  for (const tag of CONFIG.replaceTags) {
+    const startTag = tag.from
+    const endTag = `</${tag.from.slice(1, tag.from.indexOf(' '))}>`
+    const replacementStartTag = tag.to
+    const replacementEndTag = `</${tag.to.slice(1, tag.to.indexOf(' '))}>`
+    const regex = new RegExp(`(${startTag})(.*?)(${endTag})`, 'g')
+    cleanedText = cleanedText.replace(regex, `${replacementStartTag}$2${replacementEndTag}`)
+  }
+
+  return cleanedText
+}
+
+/**
+ * Replace column value by specified dividers.
+ *
+ * @param {string} text value to be cleaned
+ * @returns
+ */
+const divideBy = (text) => {
+  if (CONFIG.divideBy.length === 0) {
+    return text
+  }
+
+  for (const divider of CONFIG.divideBy) {
+  const colonPosition = text.indexOf(divider[0])
 
   if (colonPosition === -1) {
     return text
   }
 
-  const firstHalf = text.substring(0, colonPosition)
-  const secondHalf = text.substring(colonPosition + 2)
+  const firstHalf = text.substring(0, colonPosition).trim()
+  const secondHalf = text.substring(colonPosition + 2).trim()
 
-  return `${firstHalf}<appendix>${secondHalf}</appendix>`
+  return `${firstHalf} <${divider[1]}>${secondHalf}</${divider[1]}>`
+  }
 }
 
-const appendAdditionalTags = (text) => {
-  let cleanedText = cleanStandardFormatting(text)
-  cleanedText = appendAppendix(cleanedText)
-  cleanedText = appendTopicTags(cleanedText)
+const cleanAndFormatColumnValue = (text) => {
+  let cleanedText = stripTags(text)
+  cleanedText = replaceTags(cleanedText)
+  cleanedText = divideBy(cleanedText)
 
   return cleanedText
 }
 
-for (let rowNum = range.s.r; rowNum <= range.e.r; rowNum++) {
-  const firstCell = sheet[xlsx.utils.encode_cell({ r: rowNum, c: 0 })]
-  const secondCell = sheet[xlsx.utils.encode_cell({ r: rowNum, c: 1 })]
+// Read the input file
+try {
+  const file = xlsx.readFile(INPUT_FILE)
+  const sheet = file.Sheets[file.SheetNames[0]]
+  const range = xlsx.utils.decode_range(sheet['!ref'])
+  const code = []
 
-  console.log(`<entry>${appendAdditionalTags(firstCell.h)}</entry>`, `<entry>${appendAdditionalTags(secondCell.h)}</entry>`)
+  for (let rowNum = range.s.r; rowNum <= range.e.r; rowNum++) {
+    const firstCell = sheet[xlsx.utils.encode_cell({ r: rowNum, c: 0 })]
+    const secondCell = sheet[xlsx.utils.encode_cell({ r: rowNum, c: 1 })]
+
+    if (firstCell && secondCell) {
+      const idFirstCell = `${firstCell.v.slice(0, 1).toUpperCase()}-0-${rowNum}`
+      const idSecondCell = `${firstCell.v.slice(0, 1).toUpperCase()}-1-${rowNum}`
+
+      code.push(`    <${CONFIG.rowTagName} id="${idFirstCell}" corresp="${idSecondCell}" xml:lang="cat">${cleanAndFormatColumnValue(firstCell.h)}</${CONFIG.rowTagName}>\n    <${CONFIG.rowTagName} id="${idSecondCell}" corresp="${idFirstCell}" xml:lang="cze">${cleanAndFormatColumnValue(secondCell.h)}</${CONFIG.rowTagName}>\n`)
+    }
+  }
+
+  // Create the output file
+  let xml = `<?xml version="1.0" encoding="UTF-8" ?>\n  <${CONFIG.parentTagName}>\n`
+  code.map(line => {
+    xml += line
+  })
+  xml += `  </${CONFIG.parentTagName}>\n</xml>`
+
+  fs.writeFileSync(OUTPUT_FILE, xml)
+  console.info(`Created ${OUTPUT_FILE}`)
+  exit(0)
+} catch (err) {
+  console.error(err.message)
+  exit(1)
 }
